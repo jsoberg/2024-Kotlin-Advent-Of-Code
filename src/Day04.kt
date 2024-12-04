be6943bf91f086689e9eb06910a1aa9c9f76b438
Joshua Soberg <soberg.dev@pm.me>
2024-12-04 09:40:46 -0500

    Day 4 part 2 solution

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,16 +1,19 @@
 import Day04.Part1
+import Day04.Part2
 
 // https://adventofcode.com/2024/day/4
 fun main() {
     val input = readInput(day = 4)
     println("Part 1: ${Part1.calculateNumberOfWords(input)}")
+    println("Part 2: ${Part2.calculateNumberOfXmasCrosses(input)}")
 }
 
 private object Day04 {
-    private const val Word = "XMAS"
-    private const val ReverseWord = "SAMX"
 
     object Part1 {
+        private const val Word = "XMAS"
+        private const val ReverseWord = "SAMX"
+
         fun calculateNumberOfWords(input: List<String>): Int {
             var sum = 0
             for (row in 0..input.lastIndex) {
@@ -74,6 +77,48 @@ private object Day04 {
                     1
                 } else 0
             } else 0
+        }
+    }
+
+    object Part2 {
+        private const val Word = "MAS"
+        private const val ReverseWord = "SAM"
+
+        fun calculateNumberOfXmasCrosses(input: List<String>): Int {
+            var sum = 0
+            for (row in 0..input.lastIndex) {
+                for (col in 0..<input[row].length) {
+                    if (xmasCrossCheckForGridStep(input, col, row)) {
+                        sum++
+                    }
+                }
+            }
+            return sum
+        }
+
+        private fun xmasCrossCheckForGridStep(input: List<String>, col: Int, row: Int): Boolean =
+            rightDiagonalXmasCheck(input, col, row) &&
+                    leftDiagonalXmasCheck(input, col, row)
+
+        private fun rightDiagonalXmasCheck(input: List<String>, col: Int, row: Int): Boolean {
+            val endRow = row + 2
+            val endCol = col + 2
+            return if (endRow < input.size && endCol < input[row].length) {
+                val word =
+                    "${input[row][col]}${input[row + 1][col + 1]}${input[row + 2][col + 2]}"
+                word == Word || word == ReverseWord
+            } else false
+        }
+
+        private fun leftDiagonalXmasCheck(input: List<String>, col: Int, row: Int): Boolean {
+            val endRow = row + 2
+            // For the X, we need to move 2 columns over from current start.
+            val startCol = col + 2
+            return if (endRow < input.size && startCol < input[row].length) {
+                val word =
+                    "${input[row][col + 2]}${input[row + 1][col + 1]}${input[row + 2][col]}"
+                word == Word || word == ReverseWord
+            } else false
         }
     }
 }
